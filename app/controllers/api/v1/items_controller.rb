7b94a9a135a11f97887c9fe9f16ef730e6943c54
uoshvis <uoshvis@gmail.com>
2024-07-15 15:11:56 +0300

    feat: add index show create controllers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,37 @@
 class Api::V1::ItemsController < ApplicationController
-    def index
-    end
+  def index
+    items = Item.all
+    render json: items, status: 200
+  end
 
-    def show
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item, status: 200
+    else
+      render json: {error: "Item not found"}
     end
+  end
 
-    def create
+  def create
+    item = Item.new(
+      title: item_params[:title],
+      description: item_params[:description],
+      price: item_params[:price],
+    )
+    if item.save
+      render json: item, status: 200
+    else
+      render json: {error: "Error creating item"}
     end
+  end
 
+  private
+    def item_params
+      params.require(:item).permit([
+        :title,
+        :description,
+        :price
+      ])
+    end
 end
