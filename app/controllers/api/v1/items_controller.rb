b290af27e97cd8399e4441c22d3feaa09d04c079
uoshvis <uoshvis@gmail.com>
2024-07-18 11:10:15 +0300

    feat: add save! to raise  ActiveRecord::RecordInvalid exception

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -17,15 +17,11 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(
-      title: item_params[:title],
-      description: item_params[:description],
-      price: item_params[:price],
-    )
+    item = Item.new(item_params)
 
     item.user_id = current_user.id
     
-    if item.save
+    if item.save!
       render json: item, status: 200
     else
       render json: {error: "Error creating item"}
