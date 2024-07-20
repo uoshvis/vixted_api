5548c2052c326adde467a7ad58766a427c9d3b54
uoshvis <uoshvis@gmail.com>
2024-07-20 15:22:27 +0300

    tests: add initial factory objs

---
+++ test/factories/factories.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    username { "factory_boy" }
+    password {"pass1"}
+    email {"mail@mail.com"}
+  end
+end
+
+FactoryBot.define do
+  factory :item do
+    title { "factory title" }
+    description { "factory description" }
+    price {9.87}
+    user
+  end
+end
