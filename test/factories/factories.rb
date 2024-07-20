3c1d7ef6371e266bfcd7e5293b5e8d4475ae4d72
uoshvis <uoshvis@gmail.com>
2024-07-20 20:59:37 +0300

    tests: add factories

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
+  factory :item do
+    title { "factory item title" }
+    description { "factory item description" }
+    price {9.87}
+    user
+  end
+
   factory :user do
     username { "factory_boy" }
     password { "pass1" }
@@ -6,11 +13,17 @@ FactoryBot.define do
   end
 end
 
-FactoryBot.define do
-  factory :item do
-    title { "factory title" }
-    description { "factory description" }
-    price {9.87}
-    user
+def user_with_items(item_count: 5)
+  FactoryBot.create(:user) do |user|
+    FactoryBot.create_list(:item, item_count, user: user)
+  end
+end
+
+def generate_users
+  FactoryBot.build_list(:user, 10) do |user, i|
+    user.username = "username#{i}" 
+    user.email = "username#{i}@mail.com"
+    user.password = "username#{i}"
+    user.save
   end
 end
