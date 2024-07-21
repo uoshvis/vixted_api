edd72536d0ccd7e50b33af10cc02298af00dc89e
uoshvis <uoshvis@gmail.com>
2024-07-21 16:57:12 +0300

    tests: update factories

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -7,23 +7,25 @@ FactoryBot.define do
   end
 
   factory :user do
-    username { "factory_boy" }
-    password { "pass1" }
-    email { "mail@mail.com" }
+    sequence(:username) { |n| "user#{n}" }
+    sequence(:email) { |n| "user#{n}@mail.com" }
+    password { "pass" }
   end
 end
 
-def user_with_items(item_count: 5)
+# create a user with items
+def generate_user_with_items(item_count: 5)
   FactoryBot.create(:user) do |user|
     FactoryBot.create_list(:item, item_count, user: user)
   end
 end
 
+# generate 10 users
 def generate_users
   FactoryBot.build_list(:user, 10) do |user, i|
     user.username = "username#{i}" 
     user.email = "username#{i}@mail.com"
-    user.password = "username#{i}"
+    user.password = "pass"
     user.save
   end
 end
