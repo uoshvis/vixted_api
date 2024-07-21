5c51874ee2bdea3f945cb940aca77c97f5853b3f
uoshvis <uoshvis@gmail.com>
2024-07-21 16:46:27 +0300

    fix: add user email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+    format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :password, presence: true
 
   has_many :items, dependent: :destroy
