04838c30570940455c11acf197874c504e229a70
uoshvis <uoshvis@gmail.com>
2024-07-23 16:02:57 +0300

    feat: render deploy settings

--- config/puma.rb
+++ config/puma.rb
@@ -31,5 +31,13 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+preload_app!
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
